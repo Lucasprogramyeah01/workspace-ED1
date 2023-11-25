fdb6bf7bee0c28dcd24eb8c61ec0633d06f8edf3
Lucas <falla.urluc24@triana.salesianos.edu>
2023-11-25 15:15:02 +0100

    Primer proyecto java creado

---
+++ ProyectoPruebaED1/src/EEgitEnCasa/Principal.java
@@ -0,0 +1,12 @@
+package EEgitEnCasa;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("¡Hola! Este es mi primer proyecto de Java en un repositorio. :P");	
+		
+	}
+
+}
---
+++ ProyectoPruebaED1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProyectoPruebaED1 {
+}
